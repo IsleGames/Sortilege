aa1c407dcf7f96fb99d02628c1dac6e0dbf92a0b
leond <lad258@cornell.edu>
2020-02-05 13:13:23 -0600

    cards move to the right places after being dropped

---
+++ Assets/Scripts/UI/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MoveTo : MonoBehaviour
+{
+    public Transform destination;
+    public float duration = 0.1f;
+
+
+    IEnumerator MoveCard(GameObject g)
+    {
+        Vector3 init = g.transform.position;
+        float t = 0f;
+        while (t < duration)
+        {
+            float i = t / duration;
+            Debug.Log(i);
+            g.transform.SetPositionAndRotation(i * destination.position + (1f - i) * init,
+                g.transform.rotation);
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+    
+
+}
