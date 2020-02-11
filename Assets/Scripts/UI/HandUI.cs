5dfc722ade2b860952b8ad84f3b06642e56ce310
leondavis1 <lad258@cornell.edu>
2020-02-10 22:04:57 -0600

    removed TestDamage button

---
+++ Assets/Scripts/UI/HandUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class HandUI : MonoBehaviour
+{
+    int handLimit;
+    int cardnum = 0;
+    int width = 100;
+
+    public static HandUI handui;
+
+    private void Start()
+    {
+        handLimit = Game.Ctx.CardOperator.handLimit;
+        handui = this;
+    }
+
+    public void addCard(Cards.Card card)
+    {
+        if (cardnum == handLimit) return;
+        Vector3 position = new Vector3(transform.position.x, transform.position.y);
+        position.x += cardnum * width;
+        //Game.Ctx.VfxOperator.SetCardPosition(card, position);
+        cardnum += 1;
+        
+    }
+
+}
---
+++ Assets/Scripts/UI/InitHealthBars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Units;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitHealthBars: MonoBehaviour
+{
+
+    public HealthBar playerHealthBar;
+    public HealthBar enemyHealthBar;
+    // Start is called before the first frame update
+
+    public void Start()
+    { 
+        playerHealthBar.Init("Player");
+        enemyHealthBar.Init("Enemy");
+    }
+
+}
