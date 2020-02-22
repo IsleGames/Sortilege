061968db73884419424578e7d59d09a283b0a56e
QifanYang-bw <qifanyangbw@gmail.com>
2020-02-22 00:36:53 -0600

    working HP bar with Text

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,16 +1,22 @@
 using System;
 using _Editor;
 using Cards;
+using TMPro;
 using Units;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace UI
 {
     public class HealthBar : MonoBehaviour
     {
-        private RectTransform _redBar, _blueBar, _whiteBar;
-        private Health _pHealth;
+        private RectTransform _bgBar, _redBar, _blueBar, _whiteBar;
+        private TextMeshProUGUI _barText;
+        public Health pHealth;
+
+        private Rect _thisRect;
 
         private void Start()
         {
@@ -18,6 +24,7 @@ namespace UI
                 switch (tr.name)
                 {
                     case "Background":
+                        _bgBar = tr.GetComponent<RectTransform>();
                         break;
                     case "RedBar":
                         _redBar = tr.GetComponent<RectTransform>();
@@ -28,14 +35,31 @@ namespace UI
                     case "WhiteBar":
                         _whiteBar = tr.GetComponent<RectTransform>();
                         break;
+                    case "BarTMPText":
+                        _barText = tr.GetComponent<TextMeshProUGUI>();
+                        // Debugger.Log(tr.gameObject + "'s GUI is " + _barText);
+                        break;
                 }
 
             _whiteBar.GetComponent<SpriteRenderer>().enabled = false;
             _blueBar.GetComponent<SpriteRenderer>().enabled = false;
-
-            // pHealth = GetComponentInParent<Health>();
             
-            _pHealth = GetComponent<Health>();
+            var sp = _bgBar.GetComponent<SpriteRenderer>();
+            _thisRect = GetComponent<RectTransform>().rect;
+            sp.size = new Vector2(
+                _thisRect.width,
+                _thisRect.height
+                );
+            
+            if (!pHealth)
+            {
+                pHealth = GetComponentInParent<Health>();
+                if (!pHealth)
+                {
+                    pHealth = GetComponent<Health>();
+                    if (!pHealth) throw new EntryPointNotFoundException("Health Component Not found");
+                }
+            }
         }
 
         private void Update()
@@ -45,35 +69,33 @@ namespace UI
 
         private void UpdateStatus()
         {
-            float totHitPoints = _pHealth.GetMaximumDisplayHP();
-            float hpRatio = _pHealth.hitPoints / totHitPoints;
-            
-            // Debugger.Log(_pHealth.hitPoints / totHitPoints);
-            
-            AdjustBar(hpRatio, _redBar);
+            float totHitPoints = pHealth.GetMaximumDisplayHP();
+            float hpRatio = pHealth.hitPoints / totHitPoints;
 
-            if (_pHealth.barrierHitPoints > 0f)
+            AdjustBar(hpRatio, _redBar);
+            _barText.text = $"{(int)pHealth.hitPoints + pHealth.barrierHitPoints} / {(int)totHitPoints}";
+
+            if (pHealth.barrierHitPoints > 0f)
             {
-                float barrierRatio = (_pHealth.hitPoints + _pHealth.barrierHitPoints) / totHitPoints;
+                float barrierRatio = (pHealth.hitPoints + pHealth.barrierHitPoints) / totHitPoints;
                 AdjustBar(barrierRatio, _blueBar);
             }
         }
 
         private void AdjustBar(float ratio, RectTransform bar)
         {
-            Rect thisRect = GetComponent<RectTransform>().rect;
-            
-            float newWidth = ratio * thisRect.width;
-            float xShift = (-1 + ratio) * thisRect.width * .5f;
+            float newWidth = ratio * _thisRect.width;
+            float xShift = (-1 + ratio) * _thisRect.width * .5f;
 
             var sp = bar.GetComponent<SpriteRenderer>();
             sp.enabled = true;
             sp.size = new Vector2(
                 newWidth,
-                thisRect.height * .98f
+                _thisRect.height * .98f
                 );
             
-            bar.position = new Vector3(xShift, 0f, 0f);
+            bar.anchoredPosition = new Vector3(xShift, 0f, 0f);
+            // Debugger.Log(bar.position);
         }
     }
 }
