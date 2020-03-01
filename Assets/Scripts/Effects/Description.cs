0865a983a69da558a9ae3ffda76564edb6bf7cca
Leon Davis <lad258@cornell.edu>
2020-02-29 22:58:06 -0600

    Added pair of classes to describe effects and buffs

---
+++ Assets/Scripts/Effects/Description.cs
@@ -0,0 +1,94 @@
+﻿// Describes damage, healing, and armor
+public class DamageDescription
+{
+    // Healing represented by negative damage
+    public float EnemyDamage;
+    public float SelfDamage;
+    public float EnemyArmor;
+    public float SelfArmor;
+    public bool Amplified;
+
+    public override string ToString()
+    {
+        string EnemyDamageStr = $"{EnemyDamage} damage to an enemy";
+        string SelfDamageStr = $"{SelfDamage} damage to yourself";
+        string SelfHealStr = $"{-SelfDamage} health";
+        string EnemyHealStr = $"{-EnemyDamage} health";
+        string SelfArmorStr = SelfArmor > 0 ? "Gain " : "Lose" + $"{SelfArmor} armor";
+        string EnemyArmorStr = "Enemy " + (EnemyArmor > 0 ? "gains " : "loses" + $"{EnemyArmor} armor");
+        string amplifiedStr = Amplified ? "" : "(Unamplified)";
+        string desc = "";
+
+        if (SelfDamage > 0)
+        {
+            desc += "Deal " + SelfDamageStr;
+            if (EnemyDamage > 0)
+            {
+                desc += " and " + EnemyDamageStr;
+            }
+            desc += ".";
+        }
+        else
+        {
+            if (EnemyDamage > 0)
+            {
+                desc += "Deal" + EnemyDamageStr + ".";
+            }
+        }
+        if (SelfDamage < 0)
+        {
+            desc += "Gain " + SelfHealStr;
+            if (EnemyDamage < 0)
+            {
+                desc += "and enemy gains" + EnemyHealStr + ".";
+            }
+        }
+        else
+        {
+            if (EnemyDamage < 0)
+            {
+                desc += "Enemy gains " + EnemyHealStr + ".";
+            }
+        }
+        if (SelfArmor != 0)
+        {
+            desc += SelfArmorStr + ".";
+        }
+        if (EnemyArmor != 0)
+        {
+            desc += EnemyArmorStr + ".";
+        }
+        desc += amplifiedStr;
+        return desc;
+    }
+}
+
+// Describes everything in BuffEffect.cs
+public class BuffDescription
+{
+    public int Block;
+    public int Forge;
+    public int Thorns;
+    public int Plague;
+    public int Flinch;
+    public int Voodoo;
+    public int Breeze;
+
+    public override string ToString()
+    {
+        string BlockStr = $"Enemy skips their next {Block + Flinch} attacks";
+        string ForgeStr = $"Draw {Forge} more cards next turn";
+        string ThornsStr = $"Deal {Thorns} damage back when hit";
+        string PlagueStr = $"Deal 1 damage to each enemy for the next {Plague} turns";
+        //string FlinchStr: see BlockStr
+        string VoodooStr = $"If you don't lose health this turn, gain {Voodoo} health";
+        // Breeze is still unimplemented
+        string desc = "";
+        if (Block > 0 || Flinch > 0) desc += BlockStr + ".";
+        if (Forge > 0) desc += ForgeStr + ".";
+        if (Thorns > 0) desc += ThornsStr + ".";
+        if (Plague > 0) desc += PlagueStr + ".";
+        if (Voodoo > 0) desc += VoodooStr + ".";
+        return desc;
+    }
+}
