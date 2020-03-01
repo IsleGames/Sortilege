682e6c6fbe1a337ce925f8c93469d69eba8a119b
QifanYang-bw <qifanyangbw@gmail.com>
2020-02-29 22:39:41 -0600

    onmouseup now uses raycasting to prevent ordering problems

--- Assets/Scripts/Cards/CardEvent.cs
+++ Assets/Scripts/Cards/CardEvent.cs
@@ -52,8 +52,26 @@ namespace Cards
                 }
             }
         }
-    
+
         private void OnMouseUp()
+        {
+            Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+            RaycastHit2D[] hitArray = new RaycastHit2D[10];
+
+            int hitCount = Physics2D.RaycastNonAlloc(new Vector2(p.x, p.y), Vector3.forward, hitArray);
+
+            // Debugger.Log("hitCount: " + hitCount);
+            for (int i = 0; i < hitCount; i++)
+            {
+                RaycastHit2D hit = hitArray[i];
+                
+                // Debugger.Log(hit.transform.gameObject.name);
+                hit.collider.GetComponent<CardEvent>().RayCast2DTrigger();
+            }
+        }
+        
+        public void RayCast2DTrigger()
         {
             if (!Game.Ctx.player.waitingForAction) return;
             if (!availability) return;
